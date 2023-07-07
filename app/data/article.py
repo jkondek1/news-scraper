4bd1927a966a6656f501b433a6959a2bbbb93700
Jakub Kondek <j.kondek@telekom.com>
2023-07-07 23:53:15 +0200

    mix dataclasses with sqlalchemy objects

--- app/data/article.py
+++ app/data/article.py
@@ -1,14 +1,48 @@
+import logging
+import re
 from dataclasses import dataclass
 from datetime import datetime
 
-from app.data.keywords import Keywords
+import simplemma
+from langdetect import detect
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+logger = logging.getLogger(__name__)
+
+LANGUAGES = ['sk', 'cs', 'en']
 
 
 @dataclass
-class Article:
+class Article(Base):
+    __tablename__ = 'articles'
     """Represents an article from a news server."""
-    # TODO: add validation
-    header: str
-    url: str
-    timestamp: datetime
-    keywords: Keywords
+    header: str = Column(String, primary_key=True)
+    url: str = Column(String)
+    timestamp: datetime = Column(DateTime, default=datetime.utcnow)
+    keywords = relationship('Keyword', back_populates='article')
+
+    @hybrid_property
+    def keywords(self) -> list:
+        return self._get_keywords()
+
+    def _get_keywords(self, text: str) -> set[str]:
+        detected_lang = self._get_language_setting(text)
+        text = re.sub('[.|,|!|?|:|;|\'|\"]', ' ', text)
+        tokens = [tok.lower() for tok in text.split(' ') if tok != '']
+        if detected_lang:
+            keywords = [simplemma.lemmatize(tok, lang=detected_lang) for tok in tokens]
+        else:
+            logger.info('valid language not detected, not lematizing tokens')
+            keywords = tokens
+        return set(keywords)
+
+    @staticmethod
+    def _get_language_setting(text: str, valid_languages=LANGUAGES) -> str | None:
+        lang = detect(text)
+        if lang not in valid_languages:
+            return None
+        return lang
--- app/data/keywords.py
+++ app/data/keywords.py
@@ -1,37 +1,17 @@
 import logging
-import re
 from dataclasses import dataclass
 
-import simplemma
-from langdetect import detect
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 logger = logging.getLogger(__name__)
 
-LANGUAGES = ['sk', 'cs', 'en']
-
 
 @dataclass
-class Keywords:
+class Keyword:
     """Represents a set of keywords."""
-    keywords: set[str]
+    __tablename__ = 'keywords'
 
-    def __init__(self, text: str):
-        self.keywords = self._get_keywords(text)
-
-    def _get_keywords(self, text: str) -> set[str]:
-        detected_lang = self._get_language_setting(text)
-        text = re.sub('[.|,|!|?|:|;|\'|\"]', ' ', text)
-        tokens = [tok.lower() for tok in text.split(' ') if tok != '']
-        if detected_lang:
-            keywords = [simplemma.lemmatize(tok, lang=detected_lang) for tok in tokens]
-        else:
-            logger.info('valid language not detected, not lematizing tokens')
-            keywords = tokens
-        return set(keywords)
-
-    @staticmethod
-    def _get_language_setting(text: str, valid_languages=LANGUAGES) -> str | None:
-        lang = detect(text)
-        if lang not in valid_languages:
-            return None
-        return lang
+    url: str = Column(String, primary_key=True)
+    keyword: set[str] = Column(String)
+    article = relationship('Article', back_populates='keywords')
