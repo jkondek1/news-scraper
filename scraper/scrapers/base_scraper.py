e7cac8e534cf48b827f11ea7d98c16fff881765f
Jakub Kondek <j.kondek@telekom.com>
2023-07-08 23:55:57 +0200

    add cache filling and logs

--- scraper/scrapers/base_scraper.py
+++ scraper/scrapers/base_scraper.py
@@ -30,24 +30,31 @@ class BaseScraper(ABC):
         return url
 
     def scrape_articles(self, cache, db_handler) -> None:
+        logger.info(f'scraping using {self.__class__.__name__}')
         content = self._download_website_content()
         articles = self._parse_articles(content)
         self._save_articles(articles=articles, cache=cache, db_handler=db_handler)
 
     def _download_website_content(self):
+        logger.info('getting website content')
         response = requests.get(self.url)
         if response.status_code != 200:
             raise ConnectionError(f"Could not connect to {self.url}")
         return response.text
 
     def _save_articles(self, articles: list[Article], cache: ArticleCache, db_handler: DatabaseHandler) -> None:
+        logger.info('storing articles ...')
         unique_articles = cache.validate_if_in_cache(articles)
-        db_handler.connect()
-        db_handler.add_to_db(unique_articles)
-        self._save_keywords(unique_articles)
+        if unique_articles:
+            db_handler.connect()
+            logger.info('inserting articles to database')
+            db_handler.add_to_db(unique_articles)
+            self._save_keywords(unique_articles)
+            cache.fill(unique_articles)
 
     @staticmethod
     def _save_keywords(articles: list[Article], db_handler: DatabaseHandler):
+        logger.info('inserting keywords to database')
         for article in articles:
             db_handler.add_to_db(article.keywords)
 
